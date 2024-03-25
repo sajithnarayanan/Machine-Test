d932472535e3f9a23c5040fa6b43c79bea27c3ec
Sujith Narayanan <sujithn@faircodetech.com>
2024-03-25 12:16:35 +0530

    Add existing project files to Git

---
+++ app/screens/splashScreen.js
@@ -0,0 +1,28 @@
+import React, {useEffect} from 'react';
+import {View, Image, StyleSheet} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+var splash = require('../common/image/splash.jpg');
+const Splash = () => {
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate('signIn');
+    }, 3000);
+  });
+  const navigation = useNavigation();
+  return (
+    <View style={styles.container}>
+      <Image source={splash} style={styles.image}></Image>
+    </View>
+  );
+};
+export default Splash;
+const styles = StyleSheet.create({
+  container: {
+    height: '100%',
+    width: '100%',
+  },
+  image: {
+    height: '100%',
+    width: '100%',
+  },
+});
